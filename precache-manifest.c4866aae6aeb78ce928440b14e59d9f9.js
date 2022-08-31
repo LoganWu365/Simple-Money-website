aedb457160d12494639caf5c538e65c9122cb7f2
loganwu <loganwu365@gmail.com>
2022-08-31 15:47:46 +0800

    deploy

---
+++ precache-manifest.c4866aae6aeb78ce928440b14e59d9f9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "311b8f7a7fe5b45913c2",
+    "url": "/Simple-Money-website/css/app.e613caaf.css"
+  },
+  {
+    "revision": "ee8691dea77a9f859354",
+    "url": "/Simple-Money-website/css/chunk-vendors.a16c4353.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/Simple-Money-website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/Simple-Money-website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "d3f1174c9713d84053002212ccff1a1f",
+    "url": "/Simple-Money-website/index.html"
+  },
+  {
+    "revision": "311b8f7a7fe5b45913c2",
+    "url": "/Simple-Money-website/js/app.b97d7532.js"
+  },
+  {
+    "revision": "ee8691dea77a9f859354",
+    "url": "/Simple-Money-website/js/chunk-vendors.a1c73646.js"
+  },
+  {
+    "revision": "88107ee5242a75988ee24285d453a123",
+    "url": "/Simple-Money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Simple-Money-website/robots.txt"
+  }
+]);
